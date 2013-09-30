83a0d75234700c93491e08223fe399719fdc51b0
timsn <timsn@localhost>
2013-09-30 14:47:37 +0200

    Initialer Commit der Version von Max-Jonas

---
+++ utils.py
@@ -0,0 +1,35 @@
+# coding=utf8
+
+import tetris
+import time
+
+
+def echofunc(msg, clear):
+    print msg
+
+
+def sleep(ms):
+    time.sleep(ms/1000)
+
+
+def animate_piece_drop(s, a):
+    row = len(s)-2
+    while row >= 0 and s[row][a] == 0 and s[row][a+1] == 0:
+        echofunc("", True)
+        print_state(tetris.create_new_state(s, row, a))
+        row -= 1
+        time.sleep(0.02)
+
+
+def print_state(S):
+    for row in reversed(S):
+        out = u""
+        for i in row:
+            out += u"{} ".format(u"-" if i == 0 else u"⬛")
+        echofunc(out, False)
+
+
+def print_q_values(Q):
+    echofunc("Q values: {}".format(len(Q)), False)
+    for s, a in sorted(Q.items()):
+        echofunc("{}: {}".format(s, a), False)
