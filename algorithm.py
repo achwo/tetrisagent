ede10c14d3e0c547a305dbb9f6f3e25769bb70d0
timsn <timsn@localhost>
2013-10-14 12:01:04 +0200

    merge

--- algorithm.py
+++ algorithm.py
@@ -7,79 +7,132 @@ import utils
 game_controller = None
 
 
-class Algorithm:
-    pass
+def default_reward(self, state, action):
+    """
+    Calculate the reward for the current state-action-combination
+
+    :param state:
+    :param action:
+    :return:
+    """
+
+    row = len(state) - 1
+    # find row where the piece will land on
+    while row >= 0 and state[row][action] == 0 and state[row][
+                action + 1] == 0:
+        row -= 1
+    row += 1
+    # piece hit the roof => gameover
+    if row > len(state) - 2:
+        return (self.final_score(state), None)
+
+    new_s = self.create_new_state(state, row, action)
+    consecutive_zeros = [len(list(v)) for g, v in
+                         groupby(new_s[row], lambda x: x == 0) if g]
+
+    # possibilities depleted => calculate final score
+    if row == len(new_s) - 2:
+        if len(consecutive_zeros) <= 0 or max(consecutive_zeros) < 2:
+            return (self.final_score(new_s), new_s)
+
+    # otherwise, calculate current reward
+    if len(consecutive_zeros) > 0 and max(consecutive_zeros) < 2:
+        score_i = -10
+    elif len(consecutive_zeros) == 0:
+        score_i = 10
+    else:
+        score_i = 0
+    return (score_i, new_s)
+
+
+class Algorithm(object):
+
+    def __init__(self, reward):
+        self.reward = reward
+
+    def reward(self, state, action):
+        raise NotImplementedError()
 
 
 class TemporalDifferenceLearningWithEpsilonGreedyPolicy(Algorithm):
 
-    def choose_action(self, Q, A, s):
-        candidates = [A[0]]
-        for action in A:
-            #print("Q({}, {}): {}".format(s, action, Q[(s, action)]))
-            if Q[(s, action)] > Q[(s, candidates[0])]:
+    def __init__(self, reward=default_reward):
+        super(Algorithm, self).__init__(reward)    # todo waaht?
+
+    def choose_action(self, Q, ACTIONS, state):
+        """
+        Choose the action from ACTIONS to be executed
+
+        :param Q:       Quality-Map of the state-action-combination
+        :param ACTIONS: The available actions
+        :param state:   The current state
+        """
+
+        candidates = [ACTIONS[0]]
+        for action in ACTIONS:
+            #print("Q({}, {}): {}".format(state, action, Q[(state, action)]))
+            if Q[(state, action)] > Q[(state, candidates[0])]:
                 candidates = [action]
-            elif Q[(s, action)] == Q[(s, candidates[0])]:
+            elif Q[(state, action)] == Q[(state, candidates[0])]:
                 action in candidates or candidates.append(action)
-                #print("{}: {}".format(s, candidates))
+                #print("{}: {}".format(state, candidates))
         return random.choice(candidates)
 
-    def create_new_state(self, s, row, a):
-        s_new = []
-        for i in s:
-            s_new.append(tuple(i))
-        s_new[row] = list(s_new[row])
-        s_new[row + 1] = list(s_new[row + 1])
-        s_new[row][a] = 1
-        s_new[row][a + 1] = 1
-        s_new[row + 1][a] = 1
-        s_new[row + 1][a + 1] = 1
-        s_new[row] = tuple(s_new[row])
-        s_new[row + 1] = tuple(s_new[row + 1])
-        ret = tuple(s_new)
+    def create_new_state(self, state, row, action):
+        """
+        Creates a new state based on the previous state and the action
+
+        :param state:
+        :param row:
+        :param action:
+        :return:
+        """
+        state_new = []
+        for i in state:
+            state_new.append(tuple(i))
+        state_new[row] = list(state_new[row])            # convert last 2 rows
+        state_new[row + 1] = list(state_new[row + 1])    # to lists
+
+        # todo this part is block-shape specific or i don't understand it
+        state_new[row][action] = 1
+        state_new[row][action + 1] = 1
+        state_new[row + 1][action] = 1
+        state_new[row + 1][action + 1] = 1
+
+        state_new[row] = tuple(state_new[row])           # convert last 2 rows
+        state_new[row + 1] = tuple(state_new[row + 1])   # back to tuples
+
+        ret = tuple(state_new)
         return ret
 
-    def final_score(self, s):
-        if s is None:
+    def final_score(self, state):
+        """
+        Returns the score once the game is finished
+
+        :param state:
+        :return:
+        """
+        # todo  could be abstracted, since it would be great as a parameter
+        #       or maybe changed entirely
+        if state is None:
             return 0
-        for row in s:
+        for row in state:
+            # when at least one unfilled field, score is -100
             if len(row) != reduce(lambda x, y: x + y, row):
                 return -100
         return 100
 
-    def reward(self, s, a):
-        row = len(s) - 1
-        # find row where the piece will land on
-        while row >= 0 and s[row][a] == 0 and s[row][a + 1] == 0:
-            row -= 1
-        row += 1
-        # piece hit the roof => gameover
-        if row > len(s) - 2:
-            return (self.final_score(s), None)
+    def is_final_state(self, state):
+        """
 
-        new_s = self.create_new_state(s, row, a)
-        consecutive_zeros = [len(list(v)) for g, v in
-                             groupby(new_s[row], lambda x: x == 0) if g]
-
-        # possibilities depleted => calculate final score
-        if row == len(new_s) - 2:
-            if len(consecutive_zeros) <= 0 or max(consecutive_zeros) < 2:
-                return (self.final_score(new_s), new_s)
-
-        # otherwise, calculate current reward
-        if len(consecutive_zeros) > 0 and max(consecutive_zeros) < 2:
-            score_i = -10
-        elif len(consecutive_zeros) == 0:
-            score_i = 10
-        else:
-            score_i = 0
-        return (score_i, new_s)
-
-    def is_final_state(self, s):
-        if s is None:
+        :param state:
+        :return:
+        """
+        if state is None:
             return True
         consecutive_zeros = [len(list(v)) for g, v in
-                             groupby(s[len(s) - 1], lambda x: x == 0) if g]
+                             groupby(state[len(state) - 1], lambda x: x == 0) if
+                             g]
         return len(consecutive_zeros) <= 0 or max(consecutive_zeros) < 2
 
     # noinspection PyAugmentAssignment
@@ -89,7 +142,7 @@ class TemporalDifferenceLearningWithEpsilonGreedyPolicy(Algorithm):
         An episode ends, when the next state is a final state
 
         :param S0:              start state
-        :param Q:               Quality of the state-action-combination
+        :param Q:               Quality-Map of the state-action-combination
         :param ACTIONS          list of possible actions
         :param epsilon          epsilon greedy probability
         :param alpha:           learning rate
@@ -156,8 +209,9 @@ class TemporalDifferenceLearningWithEpsilonGreedyPolicy(Algorithm):
         optimal_strategy_found = False
         while episodes is not None and episodes_played < episodes or episodes \
             is None and not optimal_strategy_found:
-            last_state, last_score = self.episode(S0, Q, ACTIONS, epsilon, alpha,
-                                              interactive)
+            last_state, last_score = self.episode(S0, Q, ACTIONS, epsilon,
+                                                  alpha,
+                                                  interactive)
             episodes_played += 1
             if training is not None and \
                             episodes_played >= training:  # training done
