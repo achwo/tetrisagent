1cea9572230d2428dad5a8b6c1c22b0ec1a4650f
felix <felixjensn@gmail.com>
2013-10-18 18:48:53 +0200

    algorithmus refactorings, noch dabei aber lauffaehig

--- algorithms.py
+++ algorithms.py
@@ -4,8 +4,9 @@ import random
 import utils
 import world as w
 
-# will be set through its caller
 from world import World
+
+MAXIMUM_REWARD = 100
 
 game_controller = None
 
@@ -14,190 +15,14 @@ class Algorithm(object):
     pass
 
 
-class NewTDLearning(Algorithm):
-    def __init__(self, world=World(), Q_in=None):
-        self.world = world
-
-        self.epsilon = 0.0
-        start_alpha = 0.2
-        self.alpha = start_alpha
-
-        self.last_state = None
-        self.training = None
-        self.episodes_played = 0
-        self.Q = Q_in or defaultdict(int)
-
-        self.ACTIONS = range(0, w.FIELD_WIDTH - 1)
-
-        self.optimal_strategy_found = False
-
-    def play(self, episodes, interactive, verbose):
-        while (episodes is not None and self.episodes_played < episodes) or \
-                (episodes is None and not self.optimal_strategy_found):
-            last_state, last_score = self.episode(w.S0, self.Q, self.ACTIONS,
-                                                  self.epsilon, self.alpha,
-                                                  interactive)
-            self.episodes_played += 1
-
-            if self.training is not None and \
-                    self.episodes_played >= self.training:
-                #training done
-                self.epsilon = 0.0
-                self.alpha = 0.0
-            if not self.optimal_strategy_found and last_score == 100:
-                # end training
-                self.optimal_strategy_found = True
-                self.epsilon = 0.0
-                self.alpha = 0.0
-            if interactive:
-                utils.sleep(50)
-            if verbose:
-                if self.episodes_played % 1000 == 0:
-                    utils.echofunc('', True)
-                    utils.print_state(last_state)
-                    utils.echofunc(len(self.Q))
-                    utils.echofunc(self.episodes_played)
-            return self.Q, last_state, self.episodes_played
-        # todo last_state extrahieren
-        # todo episode()
-
-    def episode(self, s, q, ACTIONS, epsilon, alpha, interactive):
-        pass
-
-
 class TemporalDifferenceLearningWithEpsilonGreedyPolicy(Algorithm):
     def __init__(self, world=World()):
         self.world = world
 
-    def reward_and_end_episode(self, state, action):
-
-        row = len(state) - 1
-        # find row where the piece will land on
-        while row >= 0 and state[row][action] == 0 and state[row][
-                    action + 1] == 0:
-            row -= 1
-        row += 1
-        # piece hit the roof => gameover
-        if row > len(state) - 2:
-            return self.final_score(state), None
-
-        new_state = self.world.create_new_state(state, row, action)
-        # looks for more zero groups in a row
-        # (ex. (0, 0, 1, 1, 0, 0, 1, 1, 0) result: (2, 2, 1))
-        consecutive_zeros = [len(list(v)) for g, v in
-                             groupby(new_state[row], lambda x: x == 0) if g]
-
-        # possibilities depleted => calculate final score
-        if row == len(new_state) - 2:
-            if len(consecutive_zeros) <= 0 or max(consecutive_zeros) < 2:
-                return self.final_score(new_state), new_state
-
-        # otherwise, calculate current reward
-        score_i = self.world.calculate_reward(consecutive_zeros)
-
-        return score_i, new_state
-
-    def choose_action(self, Q, ACTIONS, state):
+    def play(self, episodes, training, width, start_epsilon, start_alpha, Q_in,
+             interactive, verbose):
         """
-        Choose the action from ACTIONS to be executed
-
-        :param Q:       Quality-Map of the state-action-combination
-        :param ACTIONS: The available actions
-        :param state:   The current state
-        """
-
-        candidates = [ACTIONS[0]]
-        for action in ACTIONS:
-            #print("Q({}, {}): {}".format(state, action, Q[(state, action)]))
-            if Q[(state, action)] > Q[(state, candidates[0])]:
-                candidates = [action]
-            elif Q[(state, action)] == Q[(state, candidates[0])]:
-                action in candidates or candidates.append(action)
-                #print("{}: {}".format(state, candidates))
-        return random.choice(candidates)
-
-    def final_score(self, state):
-        """
-        Returns the score once the episode is finished
-
-        :param state:
-        :return:
-        """
-        # todo  could be abstracted, since it would be great as a parameter
-        #       or maybe changed entirely
-        if state is None:
-            return 0
-        for row in state:
-            # when at least one unfilled field, score is -100
-            if len(row) != reduce(lambda x, y: x + y, row):
-                return -100
-        return 100
-
-    def is_final_state(self, state):
-        """
-
-        :param state:
-        :return:
-        """
-        if state is None:
-            return True
-
-        # looks something like [
-        consecutive_zeros = [len(list(v)) for g, v in
-                             groupby(state[len(state) - 1], lambda x: x == 0) if
-                             g]
-        return len(consecutive_zeros) <= 0 or max(consecutive_zeros) < 2
-
-    # noinspection PyAugmentAssignment
-    def episode(self, S0, Q, ACTIONS, epsilon, alpha, interactive):
-        """
-        Run one episode of the learning algorithm
-        An episode ends, when the next state is a final state
-
-        :param S0:              start state
-        :param Q:               Quality-Map of the state-action-combination
-        :param ACTIONS          list of possible actions
-        :param epsilon          epsilon greedy probability
-        :param alpha:           learning rate
-        :param interactive:     If True, things will be animated
-        :return:
-        """
-        gamma = 0.8     # weighting of future rewards in comparison to current
-        # reward
-        state = S0
-        last_field = state
-        while not self.is_final_state(state):
-            action = self.choose_action(Q, ACTIONS, state)
-            if random.random() < epsilon:
-                idx_a = ACTIONS.index(action)
-                action = random.choice(ACTIONS[:idx_a] + ACTIONS[(idx_a + 1):])
-            reward, next_state = self.reward_and_end_episode(state, action)
-
-            game_controller.setpos_callback(action)
-            game_controller.up_callback(None)
-
-            if interactive:
-                if next_state is not None:
-                    utils.animate_piece_drop(self.world, state, action)
-            h = (state, action)
-
-            # TD-Learning Algorithm:
-            Q[h] = Q[h] + alpha * (
-                reward + gamma * Q[
-                    (next_state, self.choose_action(Q, ACTIONS, next_state))] -
-                Q[h]
-            )
-
-            state = next_state
-            if state is not None:
-                last_field = state
-            utils.sleep(100)
-        return last_field, reward
-
-    def play(self, episodes, training, width, height, start_epsilon,
-             start_alpha, Q_in, interactive, verbose):
-        """
-        Play a game of tetris with the given parameters
+        Play a game of Tetris with the given parameters
         Temporal Difference Learning
 
         Returns a tuple (Q, last_s, episodes_played) containing the Q values,
@@ -214,34 +39,188 @@ class TemporalDifferenceLearningWithEpsilonGreedyPolicy(Algorithm):
         :param interactive:     sleep between episodes..
         :param verbose:         if True, console output will be longer
         """
-        alpha = start_alpha
-        ACTIONS = range(0, width - 1)
+        helper = self.playHelper(width, start_alpha, start_epsilon, Q_in,
+                                 training, interactive, verbose, self.world)
+        return helper.invoke(episodes)
 
-        Q = Q_in or defaultdict(int)
-        last_state = None
-        episodes_played = 0
-        epsilon = start_epsilon
-        optimal_strategy_found = False
-        while episodes is not None and episodes_played < episodes or episodes \
-                is None and not optimal_strategy_found:
-            last_state, last_score = self.episode(w.S0, Q, ACTIONS, epsilon,
-                                                  alpha,
-                                                  interactive)
-            episodes_played += 1
-            if training is not None and \
-                    episodes_played >= training:  # training done
-                epsilon = 0
-                alpha = 0
-            if not optimal_strategy_found and last_score == 100:  # end training
-                optimal_strategy_found = True
-                epsilon = 0
-                alpha = 0
-            if interactive:
-                utils.sleep(50)
-            if verbose:
-                if episodes_played % 1000 == 0:
-                    utils.echofunc("", True)
-                    utils.print_state(last_state)
-                    utils.echofunc(len(Q))
-                    utils.echofunc(episodes_played)
-        return Q, last_state, episodes_played
+    class playHelper(object):
+        def __init__(self, width, start_alpha, start_epsilon, Q_in,
+                     number_training_episodes, interactive, verbose, world):
+            self.width = width
+            self.previous_state = None
+            self.optimal_strategy_found = False
+            self.ACTIONS = range(0, self.width - 1)
+            self.epsilon = start_epsilon
+            self.alpha = start_alpha
+            self.gamma = 0.8
+            self.Q = Q_in or defaultdict(int)
+            self.episodes_played = 0
+            self.number_training_episodes = number_training_episodes
+            self.interactive = interactive
+            self.verbose = verbose
+            self.world = world
+
+        def print_output(self):
+            utils.echofunc("", True)
+            utils.print_state(self.previous_state)
+            utils.echofunc(len(self.Q))
+            utils.echofunc(self.episodes_played)
+
+        def interactive_stuff(self):
+            if self.interactive:
+                utils.sleep(500)
+
+        def verbose_stuff(self):
+            if self.verbose and self.episodes_played % 1000 == 0:
+                self.print_output()
+
+        def end_training_if_enough_episodes(self):
+            if self.training_done():
+                self.set_epsilon_and_alpha_zero()
+
+        def end_training_if_score_was_maximal(self):
+            if self.should_end_training():
+                self.end_training()
+
+        def invoke(self, number_episodes):
+            self.number_episodes = number_episodes
+            while self.not_finished_with_episodes() \
+                    or self.optimal_strategy_not_found():
+                self.previous_state, self.previous_score = self.episode()
+
+                self.end_training_if_enough_episodes()
+                self.end_training_if_score_was_maximal()
+                self.interactive_stuff()
+                self.verbose_stuff()
+            return self.Q, self.previous_state, self.episodes_played
+
+        def td_learning(self, h, next_state, reward):
+            # noinspection PyAugmentAssignment
+            self.Q[h] = self.Q[h] + self.alpha * (reward + self.gamma * self.Q[(
+                next_state, self.choose_action(next_state))] - self.Q[h])
+
+        def episode(self):
+            # reward
+            self.state = w.S0
+            last_field = self.state
+            while not self.is_final_state():
+                action = self.choose_action(self.state)
+                if random.random() < self.epsilon:
+                    idx_a = self.ACTIONS.index(action)
+                    action = random.choice(
+                        self.ACTIONS[:idx_a] + self.ACTIONS[(idx_a + 1):])
+                reward, next_state = self.reward_and_end_episode(self.state,
+                                                                 action)
+
+                game_controller.setpos_callback(action)
+                game_controller.up_callback(None)
+
+                if self.interactive:
+                    if next_state is not None:
+                        utils.animate_piece_drop(self.world, self.state, action)
+                h = (self.state, action)
+
+                self.td_learning(h, next_state, reward)
+
+                self.state = next_state
+                if self.state is not None:
+                    last_field = self.state
+                utils.sleep(100)
+
+            self.episodes_played += 1
+            return last_field, reward
+
+        def training_done(self):
+            """
+            should training phase be ended because training limit exceeded?
+            """
+            return self.number_training_episodes is not None and \
+                self.episodes_played >= self.number_training_episodes
+
+        def should_end_training(self):
+            """
+            should training be ended because previously the score was maximal?
+            """
+            return not self.optimal_strategy_found and \
+                self.previous_score == MAXIMUM_REWARD
+
+        def end_training(self):
+            self.optimal_strategy_found = True
+            self.set_epsilon_and_alpha_zero()
+
+        def set_epsilon_and_alpha_zero(self):
+            self.epsilon = 0
+            self.alpha = 0
+
+        def not_finished_with_episodes(self):
+            return self.number_episodes is not None \
+                and self.episodes_played < self.number_episodes
+
+        def optimal_strategy_not_found(self):
+            return self.number_episodes is None and not self.optimal_strategy_found
+
+        def is_final_state(self):
+            if self.state is None:
+                return True
+
+            consecutive_zeros = [len(list(v)) for g, v in
+                                 groupby(self.state[len(self.state) - 1],
+                                         lambda x: x == 0) if
+                                 g]
+            return len(consecutive_zeros) <= 0 or max(consecutive_zeros) < 2
+
+        def choose_action(self, state):
+            """
+            Choose the action from ACTIONS to be executed
+
+            :param Q:       Quality-Map of the state-action-combination
+            :param ACTIONS: The available actions
+            :param state:   The current state
+            """
+
+            candidates = [self.ACTIONS[0]]
+            for action in self.ACTIONS:
+                #print("Q({}, {}): {}".format(state, action, Q[(state, action)]))
+                if self.Q[(state, action)] > self.Q[(state, candidates[0])]:
+                    candidates = [action]
+                elif self.Q[(state, action)] == self.Q[(state, candidates[0])]:
+                    action in candidates or candidates.append(action)
+                    #print("{}: {}".format(state, candidates))
+            return random.choice(candidates)
+
+        def reward_and_end_episode(self, state, action):
+
+            row = len(state) - 1
+            # find row where the piece will land on
+            while row >= 0 and state[row][action] == 0 and state[row][
+                        action + 1] == 0:
+                row -= 1
+            row += 1
+            # piece hit the roof => gameover
+            if row > len(state) - 2:
+                return self.final_score(state), None
+
+            new_state = self.world.create_new_state(state, row, action)
+            # looks for more zero groups in a row
+            # (ex. (0, 0, 1, 1, 0, 0, 1, 1, 0) result: (2, 2, 1))
+            consecutive_zeros = [len(list(v)) for g, v in
+                                 groupby(new_state[row], lambda x: x == 0) if g]
+
+            # possibilities depleted => calculate final score
+            if row == len(new_state) - 2:
+                if len(consecutive_zeros) <= 0 or max(consecutive_zeros) < 2:
+                    return self.final_score(new_state), new_state
+
+            # otherwise, calculate current reward
+            score_i = self.world.calculate_reward(consecutive_zeros)
+
+            return score_i, new_state
+
+        def final_score(self, state):
+            if state is None:
+                return 0
+            for row in state:
+                # when at least one unfilled field, score is -100
+                if len(row) != reduce(lambda x, y: x + y, row):
+                    return -100
+            return 100
