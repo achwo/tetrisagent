cbed2e11b2c7953c65b1b02622467b078ca15f3b
felix <felixjensen@users.noreply.github.com>
2013-11-04 09:10:22 +0100

    bam

---
+++ algorithms.py
@@ -0,0 +1,231 @@
+from collections import defaultdict
+from itertools import groupby
+import random
+import utils
+import world as w
+
+from world import World
+
+MAXIMUM_REWARD = 100
+
+game_controller = None
+
+
+class Algorithm(object):
+    pass
+
+
+class TemporalDifferenceLearningWithEpsilonGreedyPolicy(Algorithm):
+
+    def __init__(self, world=World()):
+        self.world = world
+
+    def play(self, episodes, training, width, start_epsilon, start_alpha, Q_in,
+             interactive, verbose):
+        """
+        Play a game of Tetris with the given parameters
+        Temporal Difference Learning
+
+        Returns a tuple (Q, last_s, episodes_played) containing the Q values,
+        the last state and the number of episodes played.
+        :param episodes:        Number of episodes to be played. If None,
+                                run until optimal strategy is found
+        :param training:        number of training rounds (?)
+        :param start_epsilon:   used for epsilon-greedy. probability to choose
+                                an option other than the current optimum to
+                                learn new paths
+        :param start_alpha:     learning rate (weighting of new rewards in
+                                contrast to older ones
+        :param Q_in:            dictionary with q-values
+        :param interactive:     sleep between episodes..
+        :param verbose:         if True, console output will be longer
+        """
+        helper = self.playHelper(width, start_alpha, start_epsilon, Q_in,
+                                 training, interactive, verbose, self.world)
+        return helper.invoke(episodes)
+
+    class playHelper(object):
+        def __init__(self, width, start_alpha, start_epsilon, Q_in,
+                     number_training_episodes, interactive, verbose, world):
+            self.width = width
+            self.previous_state = None
+            self.optimal_strategy_found = False
+            self.ACTIONS = range(0, self.width - 1)
+            self.epsilon = start_epsilon
+            self.alpha = start_alpha
+            self.gamma = 0.8
+            self.Q = Q_in or defaultdict(int)
+            self.episodes_played = 0
+            self.number_training_episodes = number_training_episodes
+            self.interactive = interactive
+            self.verbose = verbose
+            self.world = world
+
+        def print_output(self):
+            utils.echofunc("", True)
+            utils.print_state(self.previous_state)
+            utils.echofunc(len(self.Q))
+            utils.echofunc(self.episodes_played)
+
+        def interactive_stuff(self):
+            if self.interactive:
+                utils.sleep(500)
+
+        def verbose_stuff(self):
+            if self.verbose and self.episodes_played % 1000 == 0:
+                self.print_output()
+
+        def end_training_if_enough_episodes(self):
+            if self.training_done():
+                self.set_epsilon_and_alpha_zero()
+
+        def end_training_if_score_was_maximal(self):
+            if self.should_end_training():
+                self.end_training()
+
+        def invoke(self, number_episodes):
+            self.number_episodes = number_episodes
+            while self.not_finished_with_episodes() \
+                    or self.optimal_strategy_not_found():
+                self.previous_state, self.previous_score = self.episode()
+
+                self.end_training_if_enough_episodes()
+                self.end_training_if_score_was_maximal()
+                self.interactive_stuff()
+                self.verbose_stuff()
+            return self.Q, self.previous_state, self.episodes_played
+
+        def td_learning(self, h, next_state, reward):
+            # noinspection PyAugmentAssignment
+            self.Q[h] = self.Q[h] + self.alpha * (reward + self.gamma * self.Q[(
+                next_state, self.choose_action(next_state))] - self.Q[h])
+
+        def interactive_animate_drop(self, action, next_state):
+            if self.interactive:
+                if next_state is not None:
+                    utils.animate_piece_drop(self.world, self.state, action)
+
+        def episode(self):
+            # reward
+            self.state = w.old_S0
+            last_field = self.state
+            while not self.is_final_state():
+                action = self.choose_action(self.state)
+                if random.random() < self.epsilon:
+                    idx_a = self.ACTIONS.index(action)
+                    action = random.choice(
+                        self.ACTIONS[:idx_a] + self.ACTIONS[(idx_a + 1):])
+                reward, next_state = self.reward_and_end_episode(self.state,
+                                                                 action)
+
+                self.world.game_controller.setpos_callback(action)
+                self.world.game_controller.up_callback(None)
+
+                self.interactive_animate_drop(action, next_state)
+                h = (self.state, action)
+
+                self.td_learning(h, next_state, reward)
+
+                self.state = next_state
+                if self.state is not None:
+                    last_field = self.state
+                utils.sleep(900)
+
+            self.episodes_played += 1
+            return last_field, reward
+
+        def training_done(self):
+            """
+            should training phase be ended because training limit exceeded?
+            """
+            return self.number_training_episodes is not None and \
+                self.episodes_played >= self.number_training_episodes
+
+        def should_end_training(self):
+            """
+            should training be ended because previously the score was maximal?
+            """
+            return not self.optimal_strategy_found and \
+                self.previous_score == MAXIMUM_REWARD
+
+        def end_training(self):
+            self.optimal_strategy_found = True
+            self.set_epsilon_and_alpha_zero()
+
+        def set_epsilon_and_alpha_zero(self):
+            self.epsilon = 0
+            self.alpha = 0
+
+        def not_finished_with_episodes(self):
+            return self.number_episodes is not None \
+                and self.episodes_played < self.number_episodes
+
+        def optimal_strategy_not_found(self):
+            return self.number_episodes is None and not self.optimal_strategy_found
+
+        def is_final_state(self):
+            if self.state is None:
+                return True
+
+            consecutive_zeros = [len(list(v)) for g, v in
+                                 groupby(self.state[len(self.state) - 1],
+                                         lambda x: x == 0) if
+                                 g]
+            return len(consecutive_zeros) <= 0 or max(consecutive_zeros) < 2
+
+        def choose_action(self, state):
+            """
+            Choose the action from ACTIONS to be executed
+
+            :param Q:       Quality-Map of the state-action-combination
+            :param ACTIONS: The available actions
+            :param state:   The current state
+            """
+
+            candidates = [self.ACTIONS[0]]
+            for action in self.ACTIONS:
+                #print("Q({}, {}): {}".format(state, action, Q[(state, action)]))
+                if self.Q[(state, action)] > self.Q[(state, candidates[0])]:
+                    candidates = [action]
+                elif self.Q[(state, action)] == self.Q[(state, candidates[0])]:
+                    action in candidates or candidates.append(action)
+                    #print("{}: {}".format(state, candidates))
+            return random.choice(candidates)
+
+        def reward_and_end_episode(self, state, action):
+
+            row = len(state) - 1
+            # find row where the piece will land on
+            while row >= 0 and state[row][action] == 0 and state[row][
+                        action + 1] == 0:
+                row -= 1
+            row += 1
+            # piece hit the roof => gameover
+            if row > len(state) - 2:
+                return self.final_score(state), None
+
+            new_state = self.world.create_new_state(state, row, action)
+            # looks for more zero groups in a row
+            # (ex. (0, 0, 1, 1, 0, 0, 1, 1, 0) result: (2, 2, 1))
+            consecutive_zeros = [len(list(v)) for g, v in
+                                 groupby(new_state[row], lambda x: x == 0) if g]
+
+            # possibilities depleted => calculate final score
+            if row == len(new_state) - 2:
+                if len(consecutive_zeros) <= 0 or max(consecutive_zeros) < 2:
+                    return self.final_score(new_state), new_state
+
+            # otherwise, calculate current reward
+            score_i = self.world.calculate_reward(consecutive_zeros)
+
+            return score_i, new_state
+
+        def final_score(self, state):
+            self.world.game_controller.clear_callback(None)
+            if state is None:
+                return 0
+            for row in state:
+                # when at least one unfilled field, score is -100
+                if len(row) != reduce(lambda x, y: x + y, row):
+                    return -100
+            return 100
---
+++ shapes.py
@@ -0,0 +1,198 @@
+direction_d = {"left": (-1, 0), "right": (1, 0), "down": (0, 1)}
+
+LEFT = "left"
+RIGHT = "right"
+DOWN = "down"
+
+
+class Block(object):
+    def __init__(self, id, (x, y)):
+        self.id = id
+        self.x = x
+        self.y = y
+
+    def coord(self):
+        return self.x, self.y
+
+
+class Shape(object):
+    """
+   Shapeis the  Base class for the game pieces e.g. square, T, S, Z, L,
+    reverse L and I. Shapes are constructed of blocks. 
+    """
+
+    @classmethod
+    def check_and_create(cls, board, coords, colour):
+        """
+        Check if the blocks that make the Shape can be placed in empty coords
+        before creating and returning the Shape instance. Otherwise, return
+        None.
+        """
+        #for coord in coords:
+        #    if not board.check_block( coord ):
+        #        return None
+
+        return cls(board, coords, colour)
+
+    def __init__(self, board, coords, colour):
+        """
+        Initialise theShapebase.
+        """
+        self.board = board
+        self.blocks = []
+
+        for coord in coords:
+            block = Block(self.board.add_block(coord, colour), coord)
+
+            self.blocks.append(block)
+
+    def move(self, direction):
+        """
+        Move the blocks in the direction indicated by adding (dx, dy) to the
+        current block coordinates
+        """
+        d_x, d_y = direction_d[direction]
+
+        for block in self.blocks:
+
+            x = block.x + d_x
+            y = block.y + d_y
+
+            if not self.board.check_block((x, y)):
+                return False
+
+        for block in self.blocks:
+            x = block.x + d_x
+            y = block.y + d_y
+
+            self.board.move_block(block.id, (d_x, d_y))
+
+            block.x = x
+            block.y = y
+
+        return True
+
+    def rotate(self, clockwise=True):
+        """
+        Rotate the blocks around the 'middle' block, 90-degrees. The
+        middle block is always the index 0 block in the list of blocks
+        that make up a Shape.
+        """
+        # TO DO: Refactor for DRY
+        middle = self.blocks[0]
+        rel = []
+        for block in self.blocks:
+            rel.append((block.x - middle.x, block.y - middle.y))
+
+        # to rotate 90-degrees (x,y) = (-y, x)
+        # First check that the there are no collisions or out of bounds moves.
+        for idx in xrange(len(self.blocks)):
+            rel_x, rel_y = rel[idx]
+            if clockwise:
+                x = middle.x + rel_y
+                y = middle.y - rel_x
+            else:
+                x = middle.x - rel_y
+                y = middle.y + rel_x
+
+            if not self.board.check_block((x, y)):
+                return False
+
+        for idx in xrange(len(self.blocks)):
+            rel_x, rel_y = rel[idx]
+            if clockwise:
+                x = middle.x + rel_y
+                y = middle.y - rel_x
+            else:
+                x = middle.x - rel_y
+                y = middle.y + rel_x
+
+            diff_x = x - self.blocks[idx].x
+            diff_y = y - self.blocks[idx].y
+
+            self.board.move_block(self.blocks[idx].id, (diff_x, diff_y))
+
+            self.blocks[idx].x = x
+            self.blocks[idx].y = y
+
+        return True
+
+
+class ShapeLimitedRotate(Shape):
+    """
+    This is a base class for the Shapes like the S, Z and I that don't fully
+    rotate (which would result in theShapemoving *up* one block on a 180).
+    Instead they toggle between 90 degrees clockwise and then back 90 degrees
+    anti-clockwise.
+    """
+
+    def __init__(self, board, coords, colour):
+        self.clockwise = True
+        super(ShapeLimitedRotate, self).__init__(board, coords, colour)
+
+    def rotate(self, clockwise=True):
+        """
+        Clockwise, is used to indicate if theShapeshould rotate clockwise
+        or back again anti-clockwise. It is toggled.
+        """
+        super(ShapeLimitedRotate, self).rotate(clockwise=self.clockwise)
+        if self.clockwise:
+            self.clockwise = False
+        else:
+            self.clockwise = True
+
+
+class SquareShape(Shape):
+    @classmethod
+    def check_and_create(cls, board):
+        coords = [(4, 0), (5, 0), (4, 1), (5, 1)]
+        return super(SquareShape, cls).check_and_create(board, coords, "red")
+
+    def rotate(self, clockwise=True):
+        """
+        Override the rotate method for the squareShapeto do exactly nothing!
+        """
+        pass
+
+
+class TShape(Shape):
+    @classmethod
+    def check_and_create(cls, board):
+        coords = [(4, 0), (3, 0), (5, 0), (4, 1)]
+        return super(TShape, cls).check_and_create(board, coords, "yellow")
+
+
+class LShape(Shape):
+    @classmethod
+    def check_and_create(cls, board):
+        coords = [(4, 0), (3, 0), (5, 0), (3, 1)]
+        return super(LShape, cls).check_and_create(board, coords, "orange")
+
+
+class ReverseLShape(Shape):
+    @classmethod
+    def check_and_create(cls, board):
+        coords = [(5, 0), (4, 0), (6, 0), (6, 1)]
+        return super(ReverseLShape, cls).check_and_create(
+            board, coords, "green")
+
+
+class ZShape(ShapeLimitedRotate):
+    @classmethod
+    def check_and_create(cls, board):
+        coords = [(5, 0), (4, 0), (5, 1), (6, 1)]
+        return super(ZShape, cls).check_and_create(board, coords, "purple")
+
+
+class SShape(ShapeLimitedRotate):
+    @classmethod
+    def check_and_create(cls, board):
+        coords = [(5, 1), (4, 1), (5, 0), (6, 0)]
+        return super(SShape, cls).check_and_create(board, coords, "magenta")
+
+
+class IShape(ShapeLimitedRotate):
+    @classmethod
+    def check_and_create(cls, board):
+        coords = [(4, 0), (3, 0), (5, 0), (6, 0)]
+        return super(IShape, cls).check_and_create(board, coords, "blue")
