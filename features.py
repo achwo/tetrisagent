36633fc8625676d329f034dc4234ddefdf1f6c4e
ChristophDe <ChristophDe@localhost>
2013-11-11 15:02:50 +0100

    Features funktionieren alle

---
+++ features.py
@@ -0,0 +1,77 @@
+import math
+from environment import FIELD_HEIGHT, FIELD_WIDTH, Environment
+
+environment = Environment()
+
+
+def max_height(environment):    
+	max_height = 0
+        for i in range(FIELD_WIDTH):
+        	for j in range(FIELD_HEIGHT):
+        		if environment.blocks[i][j] is not 0 and (FIELD_HEIGHT - j) > max_height:
+        			max_height = FIELD_HEIGHT - j
+                    break
+        	elif environment.blocks[i][j] is not 0:
+        		break
+        return max_height
+
+#the lowest column
+#return value is the height not the row
+def min_height(environment):
+	min_height = FIELD_HEIGHT
+        for i in range(FIELD_WIDTH):
+        	for j in range(FIELD_HEIGHT):
+        		if environment.blocks[i][j] is not 0 and (FIELD_HEIGHT - j) < min_height:
+        			min_height = FIELD_HEIGHT - j
+                    break
+        	elif environment.blocks[i][j] is not 0:
+        		break
+        	elif j == 11:
+        		min_height = 0
+        return min_height
+
+
+#the height of every column     
+def individual_height(environment):
+	stack = []
+        for i in range(FIELD_WIDTH):
+        	for j in range(FIELD_HEIGHT):
+        		if environment.blocks[i][j] is not 0:
+        			stack.append(FIELD_HEIGHT - j) 
+                    break 
+        	elif j == 11:
+        		stack.append(0)            
+        return stack
+
+#Hoehenunterschiede zwischen den Spalten
+def column_height_differences(environment,individual_height):
+	stack = []
+        for i in range(FIELD_WIDTH-1):
+        	stack.append(individual_height[i+1]-individual_height[i])
+        return stack
+
+#Summe der Hoehenunterschiede zwischen den Spalten
+def sum_of_column_height_differences(environment,column_height_differences):
+	sum = 0
+        for i in range(FIELD_WIDTH-1):
+        	sum += math.fabs(column_height_differences[i])
+        return sum
+
+#durchschnittliche Hoehe der Spalten    
+def mean_height(environment,individual_height):
+	sum = 0.0
+        for i in range(FIELD_WIDTH):
+        	sum += individual_height[i]
+        return sum/FIELD_WIDTH 
+
+#Anzahl der Loecher
+def number_of_holes(environment):
+	holes = 0
+        for i in range(FIELD_WIDTH):
+        	possible_hole = False
+            for j in range(FIELD_HEIGHT):
+            	if possible_hole == True and environment.blocks[i][j] is 0:
+            		holes += 1
+            	elif environment.blocks[i][j] is not 0:
+            		possible_hole=True
+        return holes
---
+++ state.py
@@ -0,0 +1,24 @@
+import copy
+
+from features import number_of_holes
+from features import min_height
+
+
+class PerceivedState(object):
+    def __init__(self, environment, *features):
+        self.features = []
+        for f in features:
+            self.features.append(f(environment))
+
+
+class SimplePerceivedState(PerceivedState):
+    def __init__(self, environment, *features):
+        super(SimplePerceivedState, self).__init__(environment, *features)
+        self.blocks = copy.deepcopy(environment.blocks)
+        self.shape = environment.current_shape
+
+
+class FirstPerceivedState(PerceivedState):
+    def __init__(self, environment, *features):
+        super(FirstPerceivedState, self).__init__(environment, number_of_holes,
+                                                  min_height, *features)
