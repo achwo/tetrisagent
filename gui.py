188d03cafff9791d5577a9abe61bd942d965bc76
Felix Jensen <felixjensen@users.noreply.github.com>
2013-11-19 23:02:14 +0100

    gui cleanups

--- gui.py
+++ gui.py
@@ -3,11 +3,11 @@
 from Tkinter import *
 import Queue
 import threading
-import tkFileDialog, Tkconstants
-
-from agent import TDLearningAgent
+import tkFileDialog
 import time
 import copy
+
+from agent import TDLearningAgent
 import settings
 import util
 
@@ -16,7 +16,10 @@ FIELD_ROWSPAN = 20
 BLOCK_SIZE_IN_PX = 30
 OFFSET_TO_WINDOW_BORDER_IN_PX = 3
 BOARD_WIDTH_IN_BLOCKS = settings.FIELD_WIDTH
+BOARD_WIDTH_IN_PX = BOARD_WIDTH_IN_BLOCKS * BLOCK_SIZE_IN_PX
 BOARD_HEIGHT_IN_BLOCKS = settings.FIELD_HEIGHT
+BOARD_HEIGHT_IN_PX = BOARD_HEIGHT_IN_BLOCKS * BLOCK_SIZE_IN_PX
+
 
 LEFT = "left"
 RIGHT = "right"
@@ -40,7 +43,6 @@ VISUALIZE_EPISODES_COUNT = 500
 STEP_SLOWDOWN_IN_SEC = 0.3
 EPISODE_SLOWDOWN_IN_SEC = 0
 
-
 global controller
 global agent
 global dataQ
@@ -48,27 +50,21 @@ dataQ = Queue.Queue(maxsize=0)
 
 
 class Board(Frame):
-
     """
     The board represents the tetris playing area. A grid of x by y blocks.
     """
 
-    def __init__(self, parent, block_size_in_px=20, board_width_in_blocks=10,
-                 board_height_in_blocks=20, offset=3):
+    def __init__(self, parent):
         Frame.__init__(self, parent)
 
-        # blocks are indexed by there corrdinates e.g. (4,5), these are
+        # blocks are indexed by there coordinates e.g. (4,5), these are
         self.landed = {}
         self.parent = parent
-        self.block_size_in_px = BLOCK_SIZE_IN_PX
-        self.board_width_in_blocks = BOARD_WIDTH_IN_BLOCKS
-        self.board_height_in_blocks = BOARD_HEIGHT_IN_BLOCKS
         self.offset = OFFSET_TO_WINDOW_BORDER_IN_PX
 
         self.canvas = Canvas(parent,
-                             height=(
-                                 self.board_height_in_blocks * self.block_size_in_px) + self.offset,
-                             width=(self.board_width_in_blocks * self.block_size_in_px) + self.offset)
+                             height=BOARD_HEIGHT_IN_PX + self.offset,
+                             width=BOARD_WIDTH_IN_PX + self.offset)
         # self.canvas.pack()
         self.canvas.grid(row=1, column=0, rowspan=FIELD_ROWSPAN)
 
@@ -76,15 +72,15 @@ class Board(Frame):
         self.canvas.delete(ALL)
         x_left = self.offset
         y_up = self.offset
-        y_bottom = self.board_height_in_blocks * self.block_size_in_px + self.offset
-        x_right = self.board_width_in_blocks * self.block_size_in_px + self.offset
-        vanish_zone_height = 2 * self.block_size_in_px + self.offset
+        y_bottom = BOARD_HEIGHT_IN_PX + self.offset
+        x_right = BOARD_WIDTH_IN_PX + self.offset
+        vanish_zone_height = 2 * BLOCK_SIZE_IN_PX + self.offset
 
         self.canvas.create_line(x_left, y_up, x_left, y_bottom)
         self.canvas.create_line(x_right, y_up, x_right, y_bottom)
         self.canvas.create_line(x_left, y_bottom, x_right, y_bottom)
         self.canvas.create_line(x_left, y_up, x_right, y_up)
-        self.canvas.create_line(x_left, vanish_zone_height, 
+        self.canvas.create_line(x_left, vanish_zone_height,
                                 x_right, vanish_zone_height,
                                 fill="red", dash=(4, 2))
 
@@ -93,20 +89,19 @@ class Board(Frame):
         Create a block by drawing it on the canvas, return
         it's ID to the caller.
         """
-        if colour == None:
+        if colour is None:
             return
-            
-        rx = (x * self.block_size_in_px) + self.offset
-        ry = (y * self.block_size_in_px) + self.offset
+
+        rx = (x * BLOCK_SIZE_IN_PX) + self.offset
+        ry = (y * BLOCK_SIZE_IN_PX) + self.offset
 
         return self.canvas.create_rectangle(
-            rx, ry, rx + self.block_size_in_px, ry + self.block_size_in_px,
+            rx, ry, rx + BLOCK_SIZE_IN_PX, ry + BLOCK_SIZE_IN_PX,
             fill=colour
         )
 
 
 class game_controller(object):
-
     """
     Main game loop and receives GUI callback events for keypresses etc...
     """
@@ -120,7 +115,6 @@ class game_controller(object):
         self.level = 0
         self.delay = 1
 
-
         self.maxLabel = Label(parent, text=MAX_BLOCKS_LABEL.format(0))
         self.maxLabel.grid(row=2, column=1, sticky=W, columnspan=3)
         self.avgLabel = Label(parent, text=AVG_BLOCKS_LABEL.format(0))
@@ -132,15 +126,15 @@ class game_controller(object):
 
         self.pauseButton = Button(parent, text=PAUSE_BUTTON_TEXT,
                                   command=self.pause_callback)
-        self.pauseButton.grid(row=FIELD_ROWSPAN-1, column=1, sticky=W)
+        self.pauseButton.grid(row=FIELD_ROWSPAN - 1, column=1, sticky=W)
 
         self.fastForwardInput = Entry(parent, width=5)
         self.fastForwardInput.insert(0, "50")
-        self.fastForwardInput.grid(row=FIELD_ROWSPAN-1, column=2, sticky=W)
+        self.fastForwardInput.grid(row=FIELD_ROWSPAN - 1, column=2, sticky=E)
 
         self.fastForwardButton = Button(parent, text=FAST_FORWARD_BUTTON_TEXT,
-                                   command=self.fast_forward_callback)
-        self.fastForwardButton.grid(row=FIELD_ROWSPAN-1, column=3, sticky=W)
+                                        command=self.fast_forward_callback)
+        self.fastForwardButton.grid(row=FIELD_ROWSPAN - 1, column=3, sticky=W)
 
         self.saveButton = Button(parent, text=SAVE_BUTTON_TEXT,
                                  command=self.save_callback)
@@ -150,27 +144,18 @@ class game_controller(object):
                                  command=self.load_callback)
         self.loadButton.grid(row=FIELD_ROWSPAN, column=2, sticky=E)
 
-
         self.quitButton = Button(parent, text=QUIT_BUTTON_TEXT,
-                            command=self.quit_callback)
-        self.quitButton.grid(row=FIELD_ROWSPAN, column=4, sticky=E)
-
+                                 command=self.quit_callback)
+        self.quitButton.grid(row=FIELD_ROWSPAN, column=4)
 
         self.options = options = {}
-        options['filetypes'] = [('all files, ','.*')]
+        options['filetypes'] = [('all files, ', '.*')]
         # options['initialdir'] = 'C:\\'
         options['initialfile'] = 'q'
         options['parent'] = tk_root
         options['title'] = "this is title"
 
-
-        self.board = Board(
-            parent,
-            block_size_in_px=BLOCK_SIZE_IN_PX,
-            board_width_in_blocks=BOARD_WIDTH_IN_BLOCKS,
-            board_height_in_blocks=BOARD_HEIGHT_IN_BLOCKS,
-            offset=OFFSET_TO_WINDOW_BORDER_IN_PX
-        )
+        self.board = Board(parent)
         self.parent.bind("<Escape>", self.quit_callback)
 
     def fast_forward_callback(self):
@@ -199,7 +184,7 @@ class game_controller(object):
             self.pauseButton['text'] = RESUME_BUTTON_TEXT
             pause_game()
 
-    def quit_callback(self, event=None):
+    def quit_callback(self):
         agent.resume_event.set()
         self.parent.quit()
 
@@ -213,20 +198,19 @@ class game_controller(object):
                 'j': 'blue',
                 'l': 'orange',
                 't': 'magenta',
-                }.get(x)
-                
+            }.get(x)
+
         self.board.clear()
         for r in range(len(blocks)):
             for c in range(len(blocks[r])):
                 color = get_color(blocks[r][c])
                 self.board.add_block((r, c), color)
 
-    def clear_callback(self, event):
+    def clear_callback(self):
         self.board.clear()
 
 
 class TDLearningAgentSlow(TDLearningAgent):
-
     """
     Special class for GUI representation with slower calculation speed
     """
@@ -309,7 +293,8 @@ def refresh_gui():
 
         controller.maxLabel["text"] = MAX_BLOCKS_LABEL.format(maximum)
         controller.avgLabel["text"] = AVG_BLOCKS_LABEL.format(avg)
-        controller.iterationsLabel["text"] = ITERATIONS_LABEL.format(agent.iterations)
+        controller.iterationsLabel["text"] = ITERATIONS_LABEL.format(
+            agent.iterations)
 
     controller.qLabel["text"] = Q_OR_NOT_LABEL.format(agent.action_from_q)
     controller.parent.after(GUI_REFRESH_IN_MS, refresh_gui)
@@ -328,7 +313,7 @@ def run(stop_event, resume_event):
 if __name__ == "__main__":
     tk_root = Tk()
     tk_root.title("tetris agent")
-    height = BOARD_HEIGHT_IN_BLOCKS * BLOCK_SIZE_IN_PX + OFFSET_TO_WINDOW_BORDER_IN_PX * 2
+    height = BOARD_HEIGHT_IN_PX + OFFSET_TO_WINDOW_BORDER_IN_PX * 2
     tk_root.minsize(450, height)
     controller = game_controller(tk_root)
     logic_stop_event = threading.Event()
