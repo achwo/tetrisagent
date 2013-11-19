4af2023d16a7e43e9059c3e57bf418a567bfdc1b
timsn <timsn@localhost>
2013-11-20 00:49:01 +0100

    Agenten Variablen alpha, gamma und epsilon sind per GUI im Pause-Modus konfigurierbar

--- gui.py
+++ gui.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
 from Tkinter import *
 import Queue
@@ -20,12 +21,12 @@ DOWN = "down"
 
 MAX_BLOCKS_LABEL = "Maximale Anzahl von Bloecken: {0}"
 AVG_BLOCKS_LABEL = "Platzierte Bloecke im Durchschnitt: {0}"
-ITERATIONS_LABEL = "Anzahl der Durchlaeufe: {0}"
+ITERATIONS_LABEL = "Anzahl der Durchläufe: {0}"
 Q_OR_NOT_LABEL = "Action aus Q: {0}"
 PAUSE_BUTTON_TEXT = "Pause"
 RESUME_BUTTON_TEXT = "Resume"
 QUIT_BUTTON_TEXT = "Quit"
-FAST_FORWARD_BUTTON_TEXT = "Fast Forward"
+FAST_FORWARD_BUTTON_TEXT = ">>"
 
 GUI_REFRESH_IN_MS = 50
 TOTAL_EPISODES = 5000
@@ -62,7 +63,7 @@ class Board(Frame):
                                  self.board_height_in_blocks * self.block_size_in_px) + self.offset,
                              width=(self.board_width_in_blocks * self.block_size_in_px) + self.offset)
         # self.canvas.pack()
-        self.canvas.grid(row=1, column=0, rowspan=5)
+        self.canvas.grid(row=0, column=0)
 
     def clear(self):
         self.canvas.delete(ALL)
@@ -96,8 +97,26 @@ class Board(Frame):
             fill=colour
         )
 
+    def update(self, blocks):
+        def get_color(x):
+            return {
+                'o': 'yellow',
+                'i': 'cyan',
+                'z': 'red',
+                's': 'green',
+                'j': 'blue',
+                'l': 'orange',
+                't': 'magenta',
+                }.get(x)
+                
+        self.clear()
+        for r in range(len(blocks)):
+            for c in range(len(blocks[r])):
+                color = get_color(blocks[r][c])
+                self.add_block((r, c), color)
 
-class game_controller(object):
+
+class Controller(object):
 
     """
     Main game loop and receives GUI callback events for keypresses etc...
@@ -112,32 +131,6 @@ class game_controller(object):
         self.level = 0
         self.delay = 1
 
-        self.fastForwardInput = Entry(parent)
-        self.fastForwardInput.insert(0, "50")
-        self.fastForwardInput.grid(row=1, column=2)
-
-
-        self.maxLabel = Label(parent, text=MAX_BLOCKS_LABEL.format(0))
-        self.maxLabel.grid(row=2, column=1, sticky=W)
-        self.avgLabel = Label(parent, text=AVG_BLOCKS_LABEL.format(0))
-        self.avgLabel.grid(row=1, column=1, sticky=W)
-        self.iterationsLabel = Label(parent, text=ITERATIONS_LABEL.format(0))
-        self.iterationsLabel.grid(row=3, column=1, sticky=W)
-        self.qLabel = Label(parent, text=Q_OR_NOT_LABEL.format('-'))
-        self.qLabel.grid(row=4, column=1, sticky=W)
-
-        self.fastForwardButton = Button(parent, text=FAST_FORWARD_BUTTON_TEXT,
-                                   command=self.fast_forward_callback)
-        self.fastForwardButton.grid(row=5, column=2, sticky=E)
-
-        self.pauseButton = Button(parent, text=PAUSE_BUTTON_TEXT,
-                                     command=self.pause_callback)
-        self.pauseButton.grid(row=5, column=1, sticky=E)
-
-        self.quitButton = Button(parent, text=QUIT_BUTTON_TEXT,
-                            command=self.quit_callback)
-        self.quitButton.grid(row=5, column=3, sticky=E)
-
         self.board = Board(
             parent,
             block_size_in_px=BLOCK_SIZE_IN_PX,
@@ -145,43 +138,90 @@ class game_controller(object):
             board_height_in_blocks=BOARD_HEIGHT_IN_BLOCKS,
             offset=OFFSET_TO_WINDOW_BORDER_IN_PX
         )
+
+        control_frame = Frame(parent)
+        control_frame.grid(row=0, column=1, sticky=N + W)
+
+        self.avgLabel = Label(control_frame, text=AVG_BLOCKS_LABEL.format(0))
+        self.avgLabel.grid(row=1, column=1, sticky=W, columnspan=3)
+        self.maxLabel = Label(control_frame, text=MAX_BLOCKS_LABEL.format(0))
+        self.maxLabel.grid(row=2, column=1, sticky=W, columnspan=3)
+        self.iterationsLabel = Label(control_frame, text=ITERATIONS_LABEL.format(0))
+        self.iterationsLabel.grid(row=3, column=1, sticky=W, columnspan=3)
+        self.qLabel = Label(control_frame, text=Q_OR_NOT_LABEL.format('-'))
+        self.qLabel.grid(row=4, column=1, sticky=W, columnspan=3)
+
+        input_width = 5
+
+        Label(control_frame, text='Fast Forward count: ').grid(row=5, column=1, sticky=E)
+        self.fastForwardInput = Entry(control_frame, width=input_width)
+        self.fastForwardInput.insert(0, "50")
+        self.fastForwardInput.grid(row=5, column=2)
+
+        Label(control_frame, text='alpha: ').grid(row=6, column=1, sticky=E)
+        self.alphaInput = Entry(control_frame, width=input_width)
+        self.alphaInput.insert(0, "0.9")
+        self.alphaInput.grid(row=6, column=2)
+
+        Label(control_frame, text='gamma: ').grid(row=7, column=1, sticky=E)
+        self.gammaInput = Entry(control_frame, width=input_width)
+        self.gammaInput.insert(0, "0.8")
+        self.gammaInput.grid(row=7, column=2)
+
+        Label(control_frame, text='epsilon: ').grid(row=8, column=1, sticky=E)
+        self.epsilonInput = Entry(control_frame, width=input_width)
+        self.epsilonInput.insert(0, "0.3")
+        self.epsilonInput.grid(row=8, column=2)
+
+        self.pauseButton = Button(control_frame, text=PAUSE_BUTTON_TEXT,
+                                     command=self.pause_callback)
+        self.pauseButton.grid(row=9, column=1, sticky=E)
+
+        self.fastForwardButton = Button(control_frame, text=FAST_FORWARD_BUTTON_TEXT,
+                                   command=self.fast_forward_callback)
+        self.fastForwardButton.grid(row=9, column=2, sticky=E)
+
+        self.quitButton = Button(control_frame, text=QUIT_BUTTON_TEXT,
+                            command=self.quit_callback)
+        self.quitButton.grid(row=9, column=3, sticky=E)
+
         self.parent.bind("<Escape>", self.quit_callback)
+
+        self._set_agent_inputs_state(DISABLED)
+
+    def _set_agent_inputs_state(self, state):
+        self.alphaInput['state'] = state
+        self.gammaInput['state'] = state
+        self.epsilonInput['state'] = state
+
+    def _set_agent_learning_vars(self):
+        alpha = float(self.alphaInput.get())
+        gamma = float(self.gammaInput.get())
+        epsilon = float(self.epsilonInput.get())
+        agent.alpha = alpha
+        agent.gamma = gamma
+        agent.epsilon = epsilon
 
     def fast_forward_callback(self):
         if not agent.fast_forward:
-            agent.fast_forward_total = int(controller.fastForwardInput.get())
+            agent.fast_forward_total = int(self.fastForwardInput.get())
             agent.fast_forward_count = agent.fast_forward_total
             agent.fast_forward = True
 
     def pause_callback(self):
         if is_game_paused():
             self.pauseButton['text'] = PAUSE_BUTTON_TEXT
+            self._set_agent_inputs_state(DISABLED)
+            self._set_agent_learning_vars()
             resume_game()
         else:
             self.pauseButton['text'] = RESUME_BUTTON_TEXT
+            self._set_agent_inputs_state(NORMAL)
             pause_game()
 
     def quit_callback(self, event=None):
         agent.resume_event.set()
         self.parent.quit()
-
-    def update_board(self, blocks):
-        def get_color(x):
-            return {
-                'o': 'yellow',
-                'i': 'cyan',
-                'z': 'red',
-                's': 'green',
-                'j': 'blue',
-                'l': 'orange',
-                't': 'magenta',
-                }.get(x)
-                
-        self.board.clear()
-        for r in range(len(blocks)):
-            for c in range(len(blocks[r])):
-                color = get_color(blocks[r][c])
-                self.board.add_block((r, c), color)
 
     def clear_callback(self, event):
         self.board.clear()
@@ -260,7 +300,7 @@ def refresh_gui():
     try:
         blocks = dataQ.get(timeout=0.1)
         if blocks:
-            controller.update_board(blocks)
+            controller.board.update(blocks)
     except:
         pass
 
@@ -292,12 +332,11 @@ if __name__ == "__main__":
     tk_root.title("tetris agent")
     height = BOARD_HEIGHT_IN_BLOCKS * BLOCK_SIZE_IN_PX + OFFSET_TO_WINDOW_BORDER_IN_PX * 2
     tk_root.minsize(450, height)
-    controller = game_controller(tk_root)
+    controller = Controller(tk_root)
     logic_stop_event = threading.Event()
     logic_resume_event = threading.Event()
-    logic_thread = threading.Thread(target=run,
-                                    args=(logic_stop_event,
-                                          logic_resume_event))
+    logic_thread = threading.Thread(target=run, 
+                                    args=(logic_stop_event, logic_resume_event))
     logic_thread.start()
     tk_root.after(GUI_REFRESH_IN_MS, refresh_gui)
     tk_root.mainloop()
