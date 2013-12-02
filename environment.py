b211af33f299caf1bf962d4705588db0ac0bfe37
timsn <timsn@localhost>
2013-12-02 11:32:02 +0100

    Shortscuts geaendert und GUI Plot Refactoring

---
+++ environment.py
@@ -0,0 +1,294 @@
+import copy
+import random
+import reward_features
+from settings import FIELD_HEIGHT, FIELD_WIDTH, VANISH_ZONE_HEIGHT
+
+BOTTOM_INDEX = FIELD_HEIGHT - 1
+RIGHTMOST_INDEX = FIELD_WIDTH - 1
+SPAWN_LOCATION = FIELD_WIDTH / 2 - 1
+BASE_SCORE_MULTIPLIER = 10
+
+
+class Environment(object):
+    def __init__(self, blocks=None):
+        self.possible_shapes = [OShape]
+        # self.possible_shapes = [OShape, JShape, IShape, LShape, ZShape, TShape,
+        #                    SShape]
+
+        self.rewards = {reward_features.game_over_reward: 10,
+                                reward_features.removed_line_reward: 2}
+
+        self.random = random.Random()
+        self.field = Field()
+        self.initialize()
+        self.field.initialize(blocks)
+        self._choose_next_shape()
+
+
+    def __eq__(self, other):
+        if type(self) == type(other):
+            return self.field == other.field and \
+                   self.current_shape == other.current_shape
+        return False
+
+    def __hash__(self):
+        return hash((self.field, self.current_shape))
+
+    def initialize(self):
+        self.highest = BOTTOM_INDEX  # counted backwards because of indexing
+        self.field.initialize()
+        self._choose_next_shape()
+        self.deleted_lines_last_round = 0
+
+    def possible_actions(self):
+        if self.is_game_over():
+            return []
+
+        actions = []
+        for column in range(0, FIELD_WIDTH):
+            if self._column_valid(column):
+                actions.append(Action(column))
+
+        return actions
+
+    def _column_valid(self, column):
+        return column + self.current_shape.rightmost <= RIGHTMOST_INDEX
+
+    def execute_action(self, action):
+        if not self._is_action_valid(action):
+            raise InvalidActionError()
+
+        self.field.place(self.current_shape, action.column)
+        self._choose_next_shape()
+        return self._calculate_reward()
+
+    def _is_action_valid(self, action):
+        return action in self.possible_actions()
+
+    def _choose_next_shape(self):
+        self.current_shape = self.random.choice(self.possible_shapes)()
+
+    def is_game_over(self):
+        return self._is_spawn_blocked() or self._is_block_in_vanish_zone()
+
+    def _is_block_in_vanish_zone(self):
+        for col in self.field.blocks:
+            for row in range(VANISH_ZONE_HEIGHT):
+                if col[row] != 0:
+                    return True
+        return False
+
+    def _is_spawn_blocked(self):
+        for coord in self.current_shape.spawn_position():
+            if self.field.blocks[coord[0]][coord[1]] != 0:
+                return True
+        return False
+
+    def _calculate_reward(self):
+        reward = 0
+
+        for feature, weighting in self.rewards.iteritems():
+            reward += BASE_SCORE_MULTIPLIER * weighting * feature(self)
+
+        return reward
+
+    def number_lines_deleted(self):
+        deleted_previously = self.deleted_lines_last_round
+        self.deleted_lines_last_round = self.field.lines_deleted
+
+        return self.field.lines_deleted - deleted_previously
+
+    def row(self, row_number):
+        row = []
+        for col in self.field.blocks:
+            row.append(col[row_number])
+        return row
+
+
+class Field(object):
+    def __init__(self):
+        self.initialize()
+
+    def __eq__(self, other):
+        if (type(self) == type(other)):
+            return self.blocks == other.blocks
+        return False
+
+    def __hash__(self):
+        return hash(tuple(self.blocks))
+
+    def initialize(self, blocks=None):
+        self.lines_deleted = 0
+        if blocks is None:
+            self.blocks = [[0 for _ in range(FIELD_HEIGHT)] for _ in
+                           range(FIELD_WIDTH)]
+        else:
+            self.blocks = blocks
+
+    def place(self, shape, column):
+        if not self._is_column_valid(column, shape):
+            raise InvalidActionError(
+                "Column {0} is not valid for shape {1}".format(column, shape))
+
+        shape.add_x_offset(column)
+        self._drop_shape(shape)
+        self._add_shape_to_field(shape)
+        self._delete_lines(self._find_full_lines())
+
+    def _drop_shape(self, shape):
+        while not self._collision(shape):
+            for coord in shape.coords:
+                coord[1] += 1
+
+    def _collision(self, shape):
+        for coord in shape.coords:
+            if self._touches_ground(coord) or self._touches_block(coord):
+                return True
+        return False
+
+    def _touches_ground(self, coord):
+        return coord[1] + 1 == FIELD_HEIGHT
+
+    def _touches_block(self, coord):
+        return self.blocks[coord[0]][coord[1] + 1] is not 0
+
+    def _add_shape_to_field(self, shape):
+        for coord in shape.coords:
+            self.blocks[coord[0]][coord[1]] = shape.__repr__()
+
+    def _is_column_valid(self, column, shape):
+        return column in self._valid_columns(shape)
+
+    def _valid_columns(self, shape):
+        valid = []
+
+        for column in range(FIELD_WIDTH):
+            if column + shape.rightmost <= RIGHTMOST_INDEX:
+                valid.append(column)
+
+        return valid
+
+    def highest_block_row(self):
+        for row in range(FIELD_HEIGHT):
+            for col in self.blocks:
+                if col[row] != 0:
+                    return row
+        return -1
+
+    def _find_full_lines(self):
+        full_lines = []
+        for row in range(FIELD_HEIGHT):
+            holes = False
+            for col in range(FIELD_WIDTH):
+                if self.blocks[col][row] == 0:
+                    holes = True
+                    break
+            if not holes:
+                full_lines.append(row)
+
+        return full_lines
+
+    def _delete_lines(self, lines):
+        for line in lines:
+            self._delete_line(line)
+
+        self.lines_deleted += len(lines)
+
+    def _delete_line(self, line):
+        for col in range(FIELD_WIDTH):
+            del self.blocks[col][line]
+            self.blocks[col].insert(0, 0)
+
+
+class Action(object):
+    def __init__(self, column):
+        self.column = column
+
+    def __eq__(self, other):
+        if type(other) is type(self):
+            return self.column == other.column
+        return False
+
+    def __hash__(self):
+        return hash(self.column)
+
+    def __repr__(self):
+        return "Action({0})".format(self.column)
+
+
+class Shape(object):
+    def __init__(self, name, coords, spawn_location=SPAWN_LOCATION):
+        self.coords = coords
+        self.rightmost = self.__furthest_right()
+        self.name = name
+        self._spawn_location = spawn_location
+
+    def __eq__(self, other):
+        if type(self) == type(other):
+            return self.name == other.name
+        return False
+
+    def __hash__(self):
+        return hash(self.name)
+
+    def __repr__(self):
+        return self.name
+
+    def __furthest_right(self):
+        maximum = 0
+        for coord in self.coords:
+            if coord[0] > maximum:
+                maximum = coord[0]
+        return maximum
+
+    def add_x_offset(self, offset):
+        for coord in self.coords:
+            coord[0] += offset
+
+    def spawn_position(self):
+        spawn = copy.deepcopy(self.coords)
+
+        for coords in spawn:
+            coords[0] += self._spawn_location
+
+        return spawn
+
+
+class OShape(Shape):
+    def __init__(self):
+        super(OShape, self).__init__('o', [[0, 0], [0, 1], [1, 0], [1, 1]],
+                                     SPAWN_LOCATION + 1)
+
+
+class IShape(Shape):
+    def __init__(self):
+        super(IShape, self).__init__('i', [[0, 0], [0, 1], [0, 2], [0, 3]])
+
+
+class LShape(Shape):
+    def __init__(self):
+        super(LShape, self).__init__('l', [[0, 0], [0, 1], [0, 2], [1, 2]])
+
+
+class JShape(Shape):
+    def __init__(self):
+        super(JShape, self).__init__('j', [[0, 2], [1, 0], [1, 1], [1, 2]])
+
+
+class TShape(Shape):
+    def __init__(self):
+        super(TShape, self).__init__('t', [[0, 1], [1, 0], [1, 1], [2, 1]])
+
+
+class SShape(Shape):
+    def __init__(self):
+        super(SShape, self).__init__('s', [[0, 0], [0, 1], [1, 1], [1, 2]])
+
+
+class ZShape(Shape):
+    def __init__(self):
+        super(ZShape, self).__init__('z', [[0, 1], [0, 2], [1, 0], [1, 1]])
+
+
+class InvalidActionError(RuntimeError):
+    pass
---
+++ environment_tests.py
@@ -0,0 +1,264 @@
+import unittest
+import copy
+
+from mock import MagicMock
+
+from environment import Environment, Action, OShape, IShape, SShape, ZShape, Field
+from environment import InvalidActionError
+import environment
+
+
+FIELD_WIDTH = 10
+FIELD_HEIGHT = 12
+BOTTOM_LINE = FIELD_HEIGHT - 1
+VANISH_ZONE_HEIGHT = 2
+
+
+class EnvironmentTests(unittest.TestCase):
+    def setUp(self):
+        self.empty_blocks = [[0 for i in range(FIELD_HEIGHT)]
+                             for j in range(FIELD_WIDTH)]
+        self.env = Environment(copy.deepcopy(self.empty_blocks))
+
+    def fill_row(self, field, row, letter):
+        for col in field:
+            col[row] = letter
+
+    def fill_row_incompletely(self, field, row, letter):
+        self.fill_row(field, row, letter)
+        field[9][row] = 0
+
+    def test_initially_game_is_not_over(self):
+        self.assertFalse(self.env.is_game_over())
+
+    def test_when_game_over_possible_actions_are_empty(self):
+        self.env.is_game_over = MagicMock(return_value=True)
+        self.assertEqual([], self.env.possible_actions())
+
+    def test_block_out(self):
+        self.env.field.blocks = self.empty_blocks
+        self.fill_row(self.env.field.blocks, 0, 'l')
+
+        self.assertTrue(self.env.is_game_over())
+
+    def test_lock_out(self):
+        self.env.blocks = self.empty_blocks
+        self.fill_row_incompletely(self.env.field.blocks, 0 + VANISH_ZONE_HEIGHT + 1, 'l')
+
+        self.env.current_shape = OShape()
+        self.assertFalse(self.env.is_game_over())
+        self.env.execute_action(Action(0))
+
+        self.assertTrue(self.env.is_game_over())
+
+    def test_execute_action_changes_field(self):
+        self.assertEqual(self.empty_blocks, self.env.field.blocks)
+
+        self.env.execute_action(Action(1))
+        self.assertNotEqual(self.empty_blocks, self.env.field.blocks)
+
+    def test_calculate_reward_calls_features(self):
+        feature1 = MagicMock(return_value=1)
+        feature2 = MagicMock(return_value=1)
+        self.env.rewards = {feature1: 1, feature2: 10}
+
+        self.env._calculate_reward()
+
+        feature1.assert_called_once_with(self.env)
+        feature2.assert_called_once_with(self.env)
+
+    def test_calculate_reward_returns_reward_based_on_features(self):
+        feature1 = MagicMock(return_value=1)
+        feature2 = MagicMock(return_value=1)
+        environment.BASE_SCORE_MULTIPLIER = 1
+        self.env.rewards = {feature1: 1, feature2: 10}
+
+        self.assertEquals(11, self.env._calculate_reward())
+
+    def test_calculate_reward_applies_weighting_and_base_multiplier(self):
+        feature = MagicMock(return_value=1)
+        environment.BASE_SCORE_MULTIPLIER = 5
+        self.env.rewards = {feature: 10}
+
+        self.assertEqual(50, self.env._calculate_reward())
+
+
+class FieldTest(unittest.TestCase):
+    def setUp(self):
+        self.empty_blocks = [[0 for i in range(FIELD_HEIGHT)]
+                             for j in range(FIELD_WIDTH)]
+
+        self.field = Field()
+
+    def assert_placed(self, shape, column, result):
+        self.field.place(shape, column)
+        self.assertEqual(result, self.field.blocks)
+
+
+    def fill_row(self, field, row, letter='p'):
+        for col in field:
+            col[row] = letter
+
+    def fill_field_row(self, row):
+        self.fill_row(self.field.blocks, row)
+
+
+    def fill_row_incompletely(self, field, row, letter):
+        self.fill_row(field, row, letter)
+        field[9][row] = 0
+
+    def test_invalid_action_throws_exception(self):
+        with self.assertRaises(InvalidActionError):
+            self.field.place(IShape(), 10)
+
+    def test_invalid_action_on_wide_shape_throws_exception(self):
+        with self.assertRaises(InvalidActionError):
+            self.field.place(OShape(), 9)
+
+    def test_o_shape_is_added_to_empty_state(self):
+        result = copy.deepcopy(self.empty_blocks)
+        result[0][BOTTOM_LINE - 1] = 'o'
+        result[0][BOTTOM_LINE] = 'o'
+        result[1][BOTTOM_LINE - 1] = 'o'
+        result[1][BOTTOM_LINE] = 'o'
+
+        self.assert_placed(OShape(), 0, result)
+
+    def test_i_shape_is_added_to_empty_state(self):
+        result = self.empty_blocks
+        result[0][BOTTOM_LINE] = 'i'
+        result[0][BOTTOM_LINE - 1] = 'i'
+        result[0][BOTTOM_LINE - 2] = 'i'
+        result[0][BOTTOM_LINE - 3] = 'i'
+
+        self.assert_placed(IShape(), 0, result)
+
+    def test_shape_is_placed_with_column_offset(self):
+        result = self.empty_blocks
+        result[3][BOTTOM_LINE] = 's'
+        result[3][BOTTOM_LINE - 1] = 's'
+        result[2][BOTTOM_LINE - 1] = 's'
+        result[2][BOTTOM_LINE - 2] = 's'
+
+        self.assert_placed(SShape(), 2, result)
+
+    def test_shape_is_placed_above_existing_blocks(self):
+        result = self.empty_blocks
+        self.fill_row_incompletely(result, BOTTOM_LINE, 'l')
+
+        self.field.blocks = copy.deepcopy(result)
+
+        result[0][BOTTOM_LINE - 1] = 'o'
+        result[0][BOTTOM_LINE - 2] = 'o'
+        result[1][BOTTOM_LINE - 1] = 'o'
+        result[1][BOTTOM_LINE - 2] = 'o'
+
+        self.assert_placed(OShape(), 0, result)
+
+    def test_shape_is_placed_partially_floating(self):
+        result = self.empty_blocks
+        self.fill_row_incompletely(result, BOTTOM_LINE, 'l')
+        result[0][BOTTOM_LINE - 1] = 'l'
+
+        self.field.blocks = copy.deepcopy(result)
+
+        result[0][BOTTOM_LINE - 2] = 'z'
+        result[0][BOTTOM_LINE - 3] = 'z'
+        result[1][BOTTOM_LINE - 3] = 'z'
+        result[1][BOTTOM_LINE - 4] = 'z'
+
+        self.assert_placed(ZShape(), 0, result)
+
+    def test_shape_is_placed_fitting_with_terrain(self):
+        result = self.empty_blocks
+        self.fill_row_incompletely(result, BOTTOM_LINE, 'l')
+        result[1][BOTTOM_LINE - 1] = 'l'
+
+        self.field.blocks = copy.deepcopy(result)
+
+        result[0][BOTTOM_LINE - 1] = 'z'
+        result[0][BOTTOM_LINE - 2] = 'z'
+        result[1][BOTTOM_LINE - 2] = 'z'
+        result[1][BOTTOM_LINE - 3] = 'z'
+
+        self.assert_placed(ZShape(), 0, result)
+
+    def test_highest_block_row_is_zero_on_empty_field(self):
+        self.assertEqual(-1, self.field.highest_block_row())
+
+    def test_highest_block_on_even_floor(self):
+        self.fill_field_row(BOTTOM_LINE)
+        self.assertEqual(BOTTOM_LINE, self.field.highest_block_row())
+
+    def test_highest_block_on_uneven_floor(self):
+        self.field.blocks[5][3] = 'l'
+        self.assertEqual(3, self.field.highest_block_row())
+
+    def test_find_no_full_line_on_field_with_holes(self):
+        self.assertEqual([], self.field._find_full_lines())
+        self.fill_field_row(BOTTOM_LINE)
+        self.fill_field_row(BOTTOM_LINE-1)
+        self.field.blocks[3][BOTTOM_LINE] = 0
+        self.field.blocks[FIELD_WIDTH-1][BOTTOM_LINE-1] = 0
+
+        self.assertEqual([], self.field._find_full_lines())
+
+    def test_find_full_lines(self):
+        self.fill_field_row(BOTTOM_LINE)
+        self.fill_field_row(BOTTOM_LINE-1)
+        self.assertEqual([BOTTOM_LINE-1, BOTTOM_LINE], self.field._find_full_lines())
+
+    def test_delete_no_lines_on_empty_list(self):
+        result = self.empty_blocks
+
+        self.fill_row(result, BOTTOM_LINE)
+        self.fill_field_row(BOTTOM_LINE)
+        self.field._delete_lines([])
+
+        self.assertEqual(result, self.field.blocks)
+
+    def test_delete_every_line_in_list(self):
+        result = self.empty_blocks
+        self.fill_row(result, BOTTOM_LINE)
+        self.fill_row(result, BOTTOM_LINE-1)
+
+        self.fill_field_row(BOTTOM_LINE)
+        self.fill_field_row(BOTTOM_LINE-1)
+        self.fill_field_row(BOTTOM_LINE-2)
+        self.fill_field_row(BOTTOM_LINE-3)
+
+        self.field._delete_lines([BOTTOM_LINE-2, BOTTOM_LINE-1])
+
+        self.assertEqual(result, self.field.blocks)
+
+    def test_delete_line_drops_a_line(self):
+        result = self.empty_blocks
+        self.fill_row(result, BOTTOM_LINE)
+        self.fill_row(result, BOTTOM_LINE-1)
+        self.fill_field_row(BOTTOM_LINE)
+        self.fill_field_row(BOTTOM_LINE-1)
+        self.fill_field_row(BOTTOM_LINE-2)
+
+        self.field._delete_lines([BOTTOM_LINE-1])
+
+        self.assertEqual(result, self.field.blocks)
+
+
+class ActionTest(unittest.TestCase):
+    def setUp(self):
+        self.a = Action(1)
+
+    def test_eq_false_cases(self):
+        other_type = 1
+        b = Action(2)
+        self.assertFalse(self.a == other_type)
+        self.assertFalse(self.a == b)
+
+    def test_eq_true_cases(self):
+        self.assertTrue(self.a == self.a)
+        b = Action(1)
+        self.assertTrue(self.a == b)
+
+
+if __name__ == '__main__':
+    unittest.main()
