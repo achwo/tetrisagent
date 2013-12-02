32241f7b3b1d5d1a63266fc2f60fc4ba44a50839
Felix Jensen <felixjensn@gmail.com>
2013-12-02 13:55:54 +0100

    state equal tests

--- agent_tests.py
+++ agent_tests.py
@@ -1,16 +1,41 @@
 import unittest
-from agent import TDLearningAgent
+import environment
+from agent import PerceivedState
 from environment import Environment
 
+from mock import MagicMock
 
-class AgentTest(unittest.TestCase):
+
+
+class PerceivedStateTest(unittest.TestCase):
+
     def setUp(self):
         self.env = Environment()
-        self.agent = TDLearningAgent()
-        self.agent.environment = self.env
+        self.env.current_shape = environment.OShape()
+        self.feature1 = MagicMock(return_value=1)
 
-    def test_on_empty_q_all_actions_are_possible(self):
-        self.assertEqual(self.env.possible_actions(),
-                         list(self.agent._find_best_actions_in_q()))
+    def test_eq_true_cases(self):
+        state1 = PerceivedState(self.env, self.feature1)
+        state2 = PerceivedState(self.env, self.feature1)
+
+        self.assertEqual(state1, state2)
+        self.assertEqual(state1, state1)
+
+    def test_eq_false_on_different_feature(self):
+        feature2 = MagicMock(return_value=2)
+
+        state1 = PerceivedState(self.env, self.feature1)
+        state2 = PerceivedState(self.env, feature2)
+
+        self.assertNotEqual(state1, state2)
+
+    def test_eq_false_on_different_shape(self):
+        state1 = PerceivedState(self.env, self.feature1)
+        self.env.current_shape = environment.IShape()
+        state2 = PerceivedState(self.env, self.feature1)
+
+        self.assertNotEqual(state1, state2)
 
 
+if __name__ == '__main__':
+    unittest.main()
